d68bc131da08efbfb57ee51d9846e5be00474fd9
Luca Bettoni Design <lucabettonidesign@gmail.com>
2024-06-27 19:39:13 +0200

    defined password generator widget basic logic and fix btn-underline style

---
+++ scripts/passwordGenerator-script.js
@@ -0,0 +1,20 @@
+const passwordBox = document.getElementById("id-password-input");
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const numbers = "0123456789";
+const symbols = "@#$%&?^*+-/=";
+const allCharacters = upperCase + lowerCase + numbers + symbols;
+
+function generatePassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+    while(length > password.length){
+        password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+    }
+
+    passwordBox.value = password;
+}
